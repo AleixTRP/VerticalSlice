8f123387cfa392bb11e70a508149b3986bf2de5b
AzureAD\ALEIXRAMIREZIPEREZ <aleix.ramirez@enti.cat>
2023-11-07 20:57:59 +0100

    Movimiento de Personage con un NullReference en los Inputs.

---
+++ Assets/Scripts/Character/Controller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    private Controller cont;
+
+    private Vector3 movementInput = Vector3.zero;
+    
+    private Vector3 finalVelocity = Vector3.zero;
+
+    [SerializeField]
+    private GameObject cam;
+
+    [SerializeField]
+    private float velocityXZ = 5f;
+
+    [SerializeField]
+    private float acceleration = 5f;
+
+
+    private void Awake()
+    {
+        cont = GetComponent<Controller>();
+
+    }
+
+    private void Update()
+    {
+        Vector2 inputVector = InputManager._INPUT_MANAGER.GetLeftAxisValue();
+        movementInput = new Vector3(inputVector.x, 0f, inputVector.y);
+        movementInput.Normalize();
+        
+        finalVelocity.x = Mathf.MoveTowards(finalVelocity.x, velocityXZ, acceleration * Time.deltaTime);
+        finalVelocity.z = Mathf.MoveTowards(finalVelocity.z, velocityXZ, acceleration * Time.deltaTime);
+
+
+    }
+}
+
+
+
+
+
+
+
